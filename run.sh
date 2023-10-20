e8804e4c5789bc7ac8a6aa5ded5e81c5494b71b7
deepak.pawar <92840095+deepak23456@users.noreply.github.com>
2023-10-20 10:20:29 +0530

    test

--- run.sh
+++ run.sh
@@ -9,5 +9,6 @@ export LAMBDA_CFT_NAME="lambda-3-lambdafuntion"
 # export LAMBDA_ZIP_NAME="lambda_3.zip"
 # export LAMBDA_ZIP_NAME="lambda_3.zip"
 
+echo "----------------------------------------"
 aws cloudformation update-stack --stack-name $LAMBDA_CFT_NAME --tags Key=Project,Value=InstaGo Key=Owner,Value=MonishParekh Key=Environment,Value=Production Key=Application,Value=InstaGo Key=Name,Value=$LAMBDA_CFT_NAME --template-body file://$LAMBDA_NAME.yml --capabilities CAPABILITY_NAMED_IAM
 
